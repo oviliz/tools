740f7926a5ace27053c4273bc84cdc592c4f342d
Cristian O. Balan <ovidio.balan@gmail.com>
2020-11-12 16:38:14 +0000

    Create Delete-FilesOlderThanX.ps1

---
+++ Delete-FilesOlderThanX.ps1
@@ -0,0 +1,15 @@
+### Task Scheduler > Start a program
+### powershell
+### -Command "& C:\PathToThisScript\Delete-FilesOlderThanX.ps1 -Directory D:\PathDo\FilesToBeDeleted -DaysToKeep 7"
+
+PARAM (
+    [parameter(Mandatory=$true)]
+    [string]$Directory,
+
+    [parameter(Mandatory=$true)]
+    [int]$DaysToKeep
+)
+
+$ThresholdDate = (Get-Date).AddDays(-$DaysToKeep)
+
+Get-ChildItem -Path $Directory | Where-Object {$_.LastWriteTime -lt $ThresholdDate} | Remove-Item -Force
